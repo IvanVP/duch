daad8917d16c764cc4d94c1f5d0ae219b591cc54
IvanVP <ipetrakov@gmail.com>
2014-05-27 11:16:59 +0400

    Another assets version

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,29 +60,7 @@ namespace :deploy do
   end
 
 
-  # namespace :assets do
-
-  #   task :precompile, :roles => :web do
-  #     from = source.next_revision(current_revision)
-  #     if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-  #       run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
-  #       run_locally "cd public && tar -jcf assets.tar.bz2 assets"
-  #       top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
-  #       run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-  #       run_locally "rm public/assets.tar.bz2"
-  #       run_locally("rake assets:clean")
-  #     else
-  #       logger.info "Skipping asset precompilation because there were no asset changes"
-  #     end
-  #   end
-
-  #   task :symlink, roles: :web do
-  #     run ("rm -rf #{latest_release}/public/assets &&
-  #           mkdir -p #{latest_release}/public &&
-  #           mkdir -p #{shared_path}/assets &&
-  #           ln -s #{shared_path}/assets #{latest_release}/public/assets")
-  #   end
-  # end
+  
 
 
 
@@ -97,6 +75,29 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
 end
 
+namespace :assets do
+
+  task :precompile, :roles => :web do
+    from = source.next_revision(current_revision)
+    if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+      run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
+      run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+      top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+      run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+      run_locally "rm public/assets.tar.bz2"
+      run_locally("rake assets:clean")
+    else
+      logger.info "Skipping asset precompilation because there were no asset changes"
+    end
+  end
+
+  task :symlink, roles: :web do
+    run ("rm -rf #{latest_release}/public/assets &&
+          mkdir -p #{latest_release}/public &&
+          mkdir -p #{shared_path}/assets &&
+          ln -s #{shared_path}/assets #{latest_release}/public/assets")
+  end
+end
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
