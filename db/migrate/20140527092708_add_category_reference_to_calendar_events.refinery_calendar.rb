1fb28e27dfcab455d2f29e4975fca58d07061a8c
IvanVP <ipetrakov@gmail.com>
2014-05-27 13:48:23 +0400

    Adds calendar

---
+++ db/migrate/20140527092708_add_category_reference_to_calendar_events.refinery_calendar.rb
@@ -0,0 +1,8 @@
+# This migration comes from refinery_calendar (originally 7)
+class AddCategoryReferenceToCalendarEvents < ActiveRecord::Migration
+
+ # def change
+ #    add_column :refinery_calendar_events, :category_id, :integer
+ #  end
+
+end
---
+++ db/migrate/20140527092709_create_event_categorizations.refinery_calendar.rb
@@ -0,0 +1,16 @@
+# This migration comes from refinery_calendar (originally 8)
+class CreateEventCategorizations < ActiveRecord::Migration
+  def self.up
+    create_table :categories_events, id: false do |t|
+      t.integer :event_id
+      t.integer :category_id
+    end
+
+    # add_index :categories_events, :refinery_calendar_event_id
+    # add_index :categories_events, :refinery_calendar_category_id
+  end
+
+  def self.down
+    drop_table :categories_events
+  end
+end
