86d3b8585457935f8ef37ade05b2b348c2ea0f35
IvanVP <ipetrakov@gmail.com>
2014-05-27 12:15:04 +0400

    Without calendar

--- db/migrate/20140521160312_add_image_id_to_calendar_events.refinery_calendar.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from refinery_calendar (originally 11)
-class AddImageIdToCalendarEvents < ActiveRecord::Migration
-
- def change
-    add_column :refinery_calendar_events, :image_id, :integer
-  end
-
-end
