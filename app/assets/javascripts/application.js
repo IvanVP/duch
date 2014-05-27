de0d9d2fd1f82ada124d9b06b2245aa7206cccf3
IvanVP <ipetrakov@gmail.com>
2014-05-27 15:34:07 +0400

    Adds rubyracer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-1.11.1.min
 //= require jquery_ujs
 //= require jquery-ui-1.10.4.min
 
